3341ed0c1de17d3e764d92b6425b8c1ffd349db1
mimiz <mmzc55@users.noreply.github.com>
2017-01-20 16:14:44 +0900

    Add files via upload

---
+++ text/tview.js
@@ -0,0 +1,16 @@
+<!--
+	var count=0;
+	function reload(){
+		count++;
+		//要素ノードオブジェクト
+		var novel=document.getElementById('novel');
+		//新たにテキストノードを作成
+		var textNode=document.createTextNode(text.substr(count-1,1));
+		//作成したテキストノードを子要素して追加する
+		novel.appendChild(textNode);
+	myTimer=setTimeout("reload()",100);
+	if(count==text.length){
+		clearTimeout(myTimer);
+		count=0;
+		}
+	}
