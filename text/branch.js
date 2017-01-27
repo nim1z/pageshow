f396668df19f1172245dda316203710158be80cd
mimiz <mmzc55@users.noreply.github.com>
2017-01-27 16:21:17 +0900

    選択肢から分岐する場合に使う

---
+++ text/branch.js
@@ -0,0 +1,19 @@
+function branch(){
+	parent.select.location.href= "https://mps2016js.github.io/dummy";
+	var s = sessionStorage.sel;
+	if (s = 1){
+		parent.text.location.href= t1;
+		parent.media.location.href= m1;
+		}
+	if (s = 2){
+		parent.text.location.href= t2;
+		parent.media.location.href= m2;
+		}
+	if (s = 3){
+		parent.text.location.href= t3;
+		parent.media.location.href= m3;
+		}
+	else{
+		alert('1`3‚ğ‘I‘ğ‚µ‚Ä‚­‚¾‚³‚¢')
+		}
+	}
