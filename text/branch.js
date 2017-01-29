cf4de123bf8d217c454e6551bb0b1eb2346eb66f
mimiz <mmzc55@users.noreply.github.com>
2017-01-29 22:28:13 +0900

    Update branch.js

--- text/branch.js
+++ text/branch.js
@@ -2,16 +2,16 @@ function branch(){
 	parent.select.location.href= "https://mps2016js.github.io/dummy.html";
 	var s = sessionStorage.sel;
 	if (s = 1){
-		parent.text.location.href= t1;
-		parent.media.location.href= m1;
+		parent.text.location.href= ta;
+		parent.media.location.href= ma;
 		}
 	if (s = 2){
-		parent.text.location.href= t2;
-		parent.media.location.href= m2;
+		parent.text.location.href= tb;
+		parent.media.location.href= mb;
 		}
 	if (s = 3){
-		parent.text.location.href= t3;
-		parent.media.location.href= m3;
+		parent.text.location.href= tc;
+		parent.media.location.href= mc;
 		}
 	else{
 		alert('1～3を選びなおせ')
