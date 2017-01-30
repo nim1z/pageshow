d1f239e05f0315f09a507d323759e181fedb6614
mimiz <mmzc55@users.noreply.github.com>
2017-01-30 13:43:16 +0900

    if文で書き直し
    履歴から掘り返せなかったのでうｐ else if

--- text/branch.js
+++ text/branch.js
@@ -1,18 +1,16 @@
 function branch(){
 	parent.select.location.href= "https://mps2016js.github.io/dummy.html";
 	var s = sessionStorage.sel;
-	switch (s){
-		case 1:
+	if (s == "1"){
 		parent.text.location.href= t1;
 		parent.media.location.href= m1;
-		break;
-		case 2:
+		}
+	else if (s == "2"){
 		parent.text.location.href= t2;
 		parent.media.location.href= m2;
-		break;
-		case 3:
+		}
+	else if (s == "3"){
 		parent.text.location.href= t3;
 		parent.media.location.href= m3;
-		break;
 		}
 	}
