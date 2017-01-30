a38f92aaa7f41970dfc75e6d9b5cad69d9c92d0c
mimiz <mmzc55@users.noreply.github.com>
2017-01-30 13:29:12 +0900

    switchで書いてみた

--- text/branch.js
+++ text/branch.js
@@ -1,16 +1,17 @@
 function branch(){
 	parent.select.location.href= "https://mps2016js.github.io/dummy.html";
 	var s = sessionStorage.sel;
-	if (s = "1"){
+	switch (s){
+		case 1:
 		parent.text.location.href= t1;
 		parent.media.location.href= m1;
-		}
-	if (s = "2"){
+		break;
+		case 2:
 		parent.text.location.href= t2;
 		parent.media.location.href= m2;
-		}
-	if (s = "3"){
+		break;
+		case 3:
 		parent.text.location.href= t3;
 		parent.media.location.href= m3;
+		break;
 		}
-	}
