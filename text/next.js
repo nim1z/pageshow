ff371a970aa90b17a22fbd78e21e82715b9c8881
nim1z <nim1z@outlook.jp>
2019-12-08 01:17:50 +0900

    Update next.js

--- text/next.js
+++ text/next.js
@@ -1,4 +1,4 @@
 function jump(){
-	parent.text.location.href= tn
-	parent.media.location.href= mn
+	parent.text.location.href= "https://nim1z.github.io/pageshow/text/" + NextText
+	parent.media.location.href= "https://nim1z.github.io/pageshow/media/" + NextMedia
 }
