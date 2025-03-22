68e9f34f809da4a4b0ad634e50f673e02619786f
nim1z <nim1z@outlook.jp>
2025-03-22 21:52:24 +0900

    メニューのレイアウト調整、テストページの調整、発行用シェルスクリプトの追加

---
+++ backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+uglifyjs ./pointrally.js -o pointrally.min.js --compress --mangle
+mv pointrally.js pointrally.js.bak
+mv pointrally.min.js pointrally.js
+7z a -r $1.zip .htaccess LICENSE.txt dummy.html favicon.ico index.html media menu.html pointrally.js select.html tag test
+mv pointrally.js pointrally.min.js
+mv pointrally.js.bak pointrally.js
